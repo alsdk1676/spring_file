38cf4066164576fd339526ebee1d5c99fe191402
alsdk1676 <alsdk6761@gmail.com>
2025-04-22 14:01:33 +0900

    fild 0422

---
+++ src/main/java/com/app/file/controller/MemberController.java
@@ -0,0 +1,18 @@
+package com.app.file.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/member/*")
+public class MemberController {
+
+    @GetMapping("my-page")
+    public void goToMyPage() {;}
+
+}
